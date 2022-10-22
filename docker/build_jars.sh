9ba5302c39e891efd4e647840113d8f1a02f4aa6
alex <aleksandr.luchko@arrival.com>
2022-10-22 18:02:55 +0300

    1) add docker to start easily in containers

---
+++ docker/build_jars.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+set -x
+
+echo "Current location: $(pwd)"
+(cd .. && mvn clean install)
+ls -alh ../
+
+#rm -rf jars/*
+echo "Move jars"
+mv ../merchant-team/target/merchant-team-1.0-SNAPSHOT.jar jars/merchant-service.jar
---
+++ docker/postgres/init_databases.sql
@@ -0,0 +1,5 @@
+grant all on schema public to postgres;
+
+-- service databases
+create database merchant;
+grant all on database merchant to postgres;
